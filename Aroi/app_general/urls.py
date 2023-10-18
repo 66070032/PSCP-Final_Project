180b08187b37e2cb3ef4cf121ce5b5ace0db1d53
Wanwisa179 <66070179@kmitl.ac.th>
2023-10-19 03:04:00 +0700

    add more page and fix color

--- Aroi/app_general/urls.py
+++ Aroi/app_general/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('food', views.landing, name='food'),
-    path('drinks', views.signin, name='drinks')
+    path('food', views.food, name='food'),
+    path('drinks', views.drinks, name='drinks'),
+    path('yourdrinks', views.yourdrinks, name='yourdrinks'),
+    path('yourfood', views.yourfood, name='yourfood')
 ]
--- Aroi/app_general/views.py
+++ Aroi/app_general/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app_general/home.html')
-def landing(request):
+def food(request):
     return render(request, 'app_general/food.html')
-def signin(request):
+def drinks(request):
     return render(request, 'app_general/drinks.html')
+def yourdrinks(request):
+    return render(request, 'app_general/yourdrinks.html')
+def yourfood(request):
+    return render(request, 'app_general/yourfood.html')
