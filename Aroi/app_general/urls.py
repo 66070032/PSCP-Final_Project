4df4875e6be9bb30552b1a760a8e150a4e10ae2a
FahTW <66070021@kmitl.ac.th>
2023-10-22 22:03:49 +0700

    change

--- Aroi/app_general/urls.py
+++ Aroi/app_general/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('landing', views.landing, name='landing'),
+    path('food', views.food, name='food'),
+    path('drinks', views.drinks, name='drinks'),
     path('signin', views.signin, name='signin')
 ]
--- Aroi/app_general/views.py
+++ Aroi/app_general/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app_general/home.html')
-def landing(request):
-    return render(request, 'app_general/landing.html')
+def food(request):
+    return render(request, 'app_general/food.html')
+def drinks(request):
+    return render(request, 'app_general/drinks.html')
 def signin(request):
     return render(request, 'app_general/signin.html')
